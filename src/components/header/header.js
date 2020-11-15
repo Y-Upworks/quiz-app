07ab250e24cdfce69b1ee1118166f056a5b46d33
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-11-15 20:25:16 +0530

    added dummy header component

---
+++ src/components/header/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="title">Quiz App</div>
+      <div className="options">
+        <div className="option">HOME</div>
+        <div className="option">ABOUT</div>
+        <div className="option">SIGN IN</div>
+        <div className="option">PROFILE</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/home/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./home.scss";
+
+const HomePage = () => {
+  return <div className="home-page">Home</div>;
+};
+
+export default HomePage;
