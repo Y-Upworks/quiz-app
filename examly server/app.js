8cccf1e9e9d5cfc80ac14ddec8017ace51a06cad
vyash5075 <vyash5075@gmail.com>
2020-11-19 10:04:44 +0530

    backend deveopment

---
+++ examly server/app.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const mongoose=require('mongoose');
+const {MONGOURI}=require('./keys');
+const auth=require('./routes/auth');
+const category=require('./routes/category');
+
+
+
+app.use(cors());
+mongoose.connect(MONGOURI,{useNewUrlParser: true,useUnifiedTopology: true})
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+mongoose.connection.on('connected',()=>{
+   console.log("mongodb connection established successfully");
+})
+mongoose.connection.on('error',()=>{
+    console.log("mongodb connection Failed");
+})
+app.use('/',auth);
+app.use('/category',category);
+
+app.listen(4000,()=>{console.log('server is running')})
